d240dfc8c852ab40d814ebe6976f45e323729306
WeidongFang1985 <fangweidong1985@gmail.com>
2024-06-01 13:26:39 +1000

    Campaign -- post a compaign done

--- app/controllers/campaigns.js
+++ app/controllers/campaigns.js
@@ -1,5 +1,5 @@
 const CampaignModel = require('../models/campaign')
-// const UserModel = require('../models/user')
+const UserModel = require('../models/user')
 
 // Find all campaigns with user detail
 exports.index = async (req, res) => {
@@ -9,4 +9,26 @@ exports.index = async (req, res) => {
 		.sort({created_at: -1})
 		.exec();
 	res.json(campaigns)
+}
+
+// Create a campaign
+exports.store = async (req, res) => {
+	const {author, title, content, photo, category} = req.body;
+	const is_approved = "pending";
+	const campaign = new CampaignModel({author, title, content, photo, category, is_approved});
+	try {
+		const savedCampaign = await campaign.save();
+		await UserModel.findByIdAndUpdate(
+			author,
+			{
+				$push: {
+					campaigns: savedCampaign._id
+				}
+			}
+		)
+		await campaign.save();
+		res.status(201).json(campaign)
+	} catch (e) {
+		res.status(400).json({message:'Lack of information'})
+	}
 }
--- app/routes/v1/campaigns.js
+++ app/routes/v1/campaigns.js
@@ -1,8 +1,9 @@
 const campaigns = require("../../controllers/campaigns");
 const {Router} = require("express");
-// const authGuard = require("../../middleware/authGuards");
+const authGuard = require("../../middleware/authGuards");
 const campaignRouter = Router();
 
 campaignRouter.get('/campaigns', campaigns.index);
+campaignRouter.post('/campaigns', authGuard, campaigns.store);
 
 module.exports = campaignRouter;
